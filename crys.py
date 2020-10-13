fad533818a1154e283c6d8bac894510ca0208418
Bart Massey <bart.massey@gmail.com>
2020-10-13 13:50:18 -0700

    completed initial search code; bugs remain

--- crys.py
+++ crys.py
@@ -21,10 +21,15 @@ class Puzzle(object):
         max_width = max([len(l) for l in lines])
         self.lines = [ ["0"] * (max_width - len(l)) + l for l in lines ]
 
-        vars = set()
-        for line in self.lines:
-            vars |= set(line)
-        self.vars = list(vars)
+        # Order the variables right-to-left top-to-bottom.
+        vars = []
+        for i in range(3):
+            for c in self.lines[i]:
+                if c == "0" or c in vars:
+                    continue
+                vars.append(c)
+
+        self.vars = vars
         self.vals = dict()
     
     def show(self):
@@ -42,5 +47,63 @@ class Puzzle(object):
                     print(c, end="")
             print()
 
+    def solve(self):
+
+        # Return 1 if puzzle is solved,
+        # 0 if puzzle is partially solved,
+        # -1 if state is illegal.
+        def check():
+
+            def value(var):
+                if var == "0":
+                    return 0;
+                if var in self.vals:
+                    return self.vals[var]
+                return None
+
+            carry = 0
+            for d1 in reversed(self.lines[0]):
+                v1 = value(d1)
+                if v1 is None:
+                    return 0
+                for d2 in reversed(self.lines[1]):
+                    v2 = value(d2)
+                    if v2 is None:
+                        return 0
+                    for s in reversed(self.lines[2]):
+                        vs = value(s)
+                        if vs is None:
+                            return 0
+                        total = v1 + v2 + carry
+                        if total % 10 != vs:
+                            return -1
+                        carry = total - total % 10
+
+            if carry != 0:
+                return -1
+            
+        unused = sorted(set(range(10)) - set(self.vals.values()))
+        for v in self.vars:
+            if v in self.vals:
+                continue
+            for val in unused:
+                self.vals[v] = val
+                status = check()
+                if status == 1:
+                    return True
+                if status == -1:
+                    del self.vals[v]
+                    continue
+                result = self.solve()
+                if result:
+                    return True
+                del self.vals[v]
+
+        if not self.vals:
+            return None
+        return self.vals
+
 puzzle = Puzzle(open(sys.argv[1], "r"))
+puzzle.show()
+print(puzzle.solve())
 puzzle.show()
