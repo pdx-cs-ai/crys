3be9bc4d9e80f38abe9c0172b7a48037e8134cef
Bart Massey <bart.massey@gmail.com>
2020-10-13 11:20:13 -0700

    wrote basic puzzle reading and printing

---
+++ crys.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# Cryptarithm solver using DFS
+# Bart Massey
+
+import sys
+
+# Read in a cryptarithm.
+class Puzzle(object):
+    
+    def __init__(self, f):
+
+        # Read in a line and split into characters.
+        def read_line():
+            line = next(f).strip()
+            return [c for c in line]
+
+        self.lines = [read_line() for _ in range(3)]
+        vars = set()
+        for line in self.lines:
+            vars |= set(line)
+        self.vars = list(vars)
+
+    
+    def show(self):
+        max_width = max([len(l) for l in self.lines])
+        for l in self.lines:
+            width = len(l)
+            for _ in range(max_width - width):
+                print(" ", end="")
+            for c in l:
+                print(c, end="")
+            print()
+
+puzzle = Puzzle(open(sys.argv[1], "r"))
+puzzle.show()
